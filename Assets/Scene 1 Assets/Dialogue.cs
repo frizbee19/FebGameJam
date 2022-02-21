d53725e3f0ad3e4cbcfcd1af922728712c9567bf
Rizvee <frizbee19@gmail.com>
2022-02-20 23:24:26 -0600

    Branch + textbox pages

--- Assets/Scene 1 Assets/Dialogue.cs
+++ Assets/Scene 1 Assets/Dialogue.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
+    public TMP_Text textbox;
+    ArrayList<string> pages;
+    int curPage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //pages to scroll through
+        pages = new ArrayList<string>();
     }
 
+    //Adds a page of text
+    void AddText(string text) 
+    {
+        pages.Add(text);
+    }
     // Update is called once per frame
     void Update()
     {
