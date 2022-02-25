b4521d989e104ac86d3740673bf7d49929757513
Rizvee <frizbee19@gmail.com>
2022-02-24 22:20:29 -0600

    Worked on first level

--- Assets/Scene 1 Assets/Movement.cs
+++ Assets/Scene 1 Assets/Movement.cs
@@ -19,7 +19,7 @@ public class Movement : MonoBehaviour
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
-	private Vector3 m_Velocity = Vector3.zero;
+	public Vector3 m_Velocity = Vector3.zero;
 	public static bool pause = false;
 	private bool dj = false;
 
