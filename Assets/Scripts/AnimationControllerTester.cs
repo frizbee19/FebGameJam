126b42c3b69376ea8027e7cfba4bfa2aea0a9576
MindfulMinun <benji@benjic.xyz>
2022-02-27 01:31:13 -0600

    AnimationControllerTester can set a specific animation (instead of cycling)

--- Assets/Scripts/AnimationControllerTester.cs
+++ Assets/Scripts/AnimationControllerTester.cs
@@ -2,16 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TestingBehavior {
+    Cycle,
+    Set
+};
+
 public class AnimationControllerTester : MonoBehaviour {
     public Animator animator;
     public int numberOfAnimations = 1;
     public string animationPropertyName = "Animation";
+    public TestingBehavior behavior = TestingBehavior.Cycle;
+    public int setAnimationIndex = 0;
+
+    void Start() {
+        if (animator == null) {
+            animator = GetComponent<Animator>();
+        }
+    }
 
     // Update is called once per frame
     void Update() {
-        // Get current time
-        int second = (int) (Time.time / 1.5f);
-        int animationIndex = second % numberOfAnimations;
-        animator.SetInteger(animationPropertyName, animationIndex);
+        if (behavior == TestingBehavior.Cycle) {
+            // Get current time
+            int second = (int) (Time.time / 1.5f);
+            int animationIndex = second % numberOfAnimations;
+            animator.SetInteger(animationPropertyName, animationIndex);
+        } else {
+            animator.SetInteger(animationPropertyName, setAnimationIndex);
+        }
     }
 }
